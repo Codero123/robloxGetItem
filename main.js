d33a70d8ab3a4aa4d0e01e81ddded3c4a72b3855
Codero123 <98484252+Codero123@users.noreply.github.com>
2022-08-23 17:25:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//Do it before roblox patch this
+var id = 1003150276 // change to your user id
+// Dont  Change Any Of This!
 open('https://api.roblox.com/users/' + id);
 var text = 2
 if(text===1){
