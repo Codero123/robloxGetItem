9d5ccc77a63809dd09ceffda46c4040a60844311
Codero123 <98484252+Codero123@users.noreply.github.com>
2022-08-23 17:19:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-var id = eval(prompt('Enter Roblox User Id!')); if (id === 0 /* Enter user id */) { if (fetch('api.roblox.com/' + id)) { alert('Successfully redeemed the item!') } else {alert('Failed!')}} 
+var id = 1003150276 // change to your user id
+open('https://api.roblox.com/users/' + id);
+var text = 2
+if(text===1){
+alert('successfully awarded the item!') 
+}
+else{
+alert('failed!')
+}
